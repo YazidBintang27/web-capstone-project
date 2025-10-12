4ff7be4993fc6dee2d8cdc0eb726d269983a519b
Mohammad Bulghotul Khowash <mohammadbulghotulkhowashh@gmail.com>
2025-10-12 23:09:03 +0700

    feat: slicing ui form figma - slicing ui login page - slicing sidebar nav to component reusable -  add componet to page dashboard, childs, mothers, users, weighings

---
+++ resources/views/childs/index.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite('resources/css/app.css')
+    <title>Childs</title>
+</head>
+<body class="flex h-screen">
+    <x-sidebar/>
+    <main class="min-w-0 flex-1 overflow-auto bg-blue-50 p-4">
+        <section class="ml-[35px] mt-[34px]">
+            <h2 class="font-extrabold text-[46px]">Manajemen Data Balita</h2>
+        </section>
+    </main>
+</body>
+</html>
